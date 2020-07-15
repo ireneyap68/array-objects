e73d25b1df7a5ad8678ac1c64b46ce72618c5f03
Irene <airine@Irenes-MacBook-Air.local>
2020-07-15 14:03:49 -0700

    [arrays] Add favorite teams

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = "Irene";
+console.log(name);
+
+var myLocation = "SF";
+console.log(myLocation);
+
+var favoriteNumber = 11;
+console.log(favoriteNumber);
+
+var teams = ["Lakers", "Warriors", "Clippers", "Blazers", "Celtics", "Nets", "Rockets"];
+console.log(teams);
